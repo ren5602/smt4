27e65f6bc98bf2fed9685d80e99357ba9e96a916
ren5602 <naziz5602@gmail.com>
2025-02-23 06:17:23 +0700

    basic-routing

---
+++ WebLanjut/Week2/PWL_2025/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/selamatdatang', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('about', function () {
+    return ('NIM: 2341720237<br>Nama: Muhammad Nur aziz');
+});
