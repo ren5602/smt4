5e8268f5b78aabdb40852995a91d2f4ba0421fe5
ren5602 <naziz5602@gmail.com>
2025-03-08 12:47:51 +0700

    prak-2.2 Not Found Exceptions

--- WebLanjut/PWL_POS/app/Http/Controllers/UserController.php
+++ WebLanjut/PWL_POS/app/Http/Controllers/UserController.php
@@ -39,10 +39,14 @@ class UserController extends Controller
         // $user = UserModel::find(1);
         // $user = UserModel::where('level_id', 1)->first();
         // $user = UserModel::firstWhere('level_id', 1);
-        $user = UserModel::findOr(20,['username','nama'], function(){
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(20,['username','nama'], function(){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
 
+        // week-4 prak 2.2
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
     }
 }
