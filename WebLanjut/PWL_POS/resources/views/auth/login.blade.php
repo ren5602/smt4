10276e65915ccc40fa43d609fbcc2d9a1891f44b
ren5602 <naziz5602@gmail.com>
2025-04-13 09:52:42 +0700

    edit title

--- WebLanjut/PWL_POS/resources/views/auth/login.blade.php
+++ WebLanjut/PWL_POS/resources/views/auth/login.blade.php
@@ -23,9 +23,9 @@
     <div class="login-box">
         <!-- /.login-logo -->
         <div class="card card-outline card-primary">
-            <div class="card-header text-center"><a href="{{ url('/') }}" class="h1"><b>Admin</b>LTE</a></div>
+            <div class="card-header text-center"><a href="{{ url('/') }}" class="h1"><b>PWL</b>POS</a></div>
             <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
+                <p class="login-box-msg">Login dulu Boss</p>
                 <form action="{{ url('login') }}" method="POST" id="form-login">
                     @csrf
                     <div class="input-group mb-3">
