60a89a427344fe98f4fdd0792f15d5b60026bb07
ren5602 <naziz5602@gmail.com>
2025-04-13 09:30:28 +0700

    tambahan profile pict

--- WebLanjut/PWL_POS/resources/views/user/edit_ajax.blade.php
+++ WebLanjut/PWL_POS/resources/views/user/edit_ajax.blade.php
@@ -23,7 +23,7 @@
         @method('PUT')
         <div id="modal-master" class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header bg-dark text-white">
                     <h5 class="modal-title" id="exampleModalLabel">Edit Data User</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
